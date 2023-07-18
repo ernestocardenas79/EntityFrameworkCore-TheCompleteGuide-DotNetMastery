177948c83302aa1300326fb63535cd044a9bbee8
Ernesto Cardenas <ernesto.cardenas79@gmail.com>
2023-07-18 08:13:49 -0700

    Configured connection string for CodingWiki Db, set default schema wk

--- CodingWiki_DataAccess/ApplicationDataContext.cs
+++ CodingWiki_DataAccess/ApplicationDataContext.cs
@@ -5,5 +5,15 @@ namespace CodingWiki_DataAccess;
 
 public class ApplicationDataContext : DbContext
 {
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=localhost;Database=CodiWiki;User Id=codiwiki_app;Password=Kr0k9c9t0!;TrustServerCertificate=true;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("wk");
+    }
+
     public DbSet<Book> Books { get; set; }
 }
